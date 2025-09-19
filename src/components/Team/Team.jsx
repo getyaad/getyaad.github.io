517cc5818f0f571b49b821d4138fea6f2d0b7f20
Tony Montes <santiago.montesb@gmail.com>
2025-09-19 11:04:38 -0500

    update hiring link

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -25,7 +25,7 @@ const Team = () => {
             </div>
           </div>
 
-          <a href="https://www.linkedin.com/in/akanksharkrishna/" target="_blank" rel="noopener noreferrer" className="founder-card hiring-card">
+          <a href="https://www.linkedin.com/jobs/view/4302123247/" target="_blank" rel="noopener noreferrer" className="founder-card hiring-card">
             <div className="founder-image">
               <div className="hiring-icon">
                 <svg width="80" height="80" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" strokeWidth="1">
